5d22bad31a0a30254076197e30919b3c952cd99f
NikitaAsmedianov <nikitaa@campus.technion.ac.il>
2026-06-29 12:02:44 +0300

    generate sinusoidal

--- helloCPP.cpp
+++ helloCPP.cpp
@@ -8,26 +8,20 @@ int main() {
     ifstream inFile("user.txt");
     string names;
     getline(inFile, names);
-    float num1, num2, num3;
-    inFile >> num1 >> num2 >> num3;
+    float Amplitude, Omega, Phi0, time_start, time_end, time_step;
+    inFile >> Amplitude >> Omega >> Phi0 >> time_start >> time_end >> time_step;
     inFile.close();
     ofstream outFile("hello.txt");
-    outFile << fixed << setprecision(2) << "Equation: " << num1 << "x^2+" << num2 << "x+" << num3 << "=0\n";
-    float D = num2 * num2 - 4 * num1 * num3;
-    outFile << "Discriminant: " << D << "\n";
-    float x1, x2;
-    if (D > 0) {
-        x1 = (-num2 + sqrt(D)) / (2 * num1);
-        x2 = (-num2 - sqrt(D)) / (2 * num1);
-        outFile << "Roots: " << x1 << ", " << x2 << "\n";
-    } else if (D == 0) {
-        x1 = -num2 / (2 * num1);
-        outFile << "Root: " << x1 << "\n";
-    } else {
-        outFile << "No real roots.\n";
+    outFile << fixed << setprecision(2);
+    outFile <<"time(s) Voltage(V)\n";
+    for (float t = time_start; t <= time_end; t += time_step)
+    {
+        float voltage = Amplitude * sin(Omega * t + Phi0);
+        outFile << t << " " << voltage << "\n";
     }
+    
     outFile.close();
     
-    cout << "Square equation processed successfully!\n";
+    cout << "Sinusoidal data generated successfully!\n";
     return 0;
 }
