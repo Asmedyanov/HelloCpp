a41ca4b7cf6957e1f63a6578ecea2bf3ccadfb68
NikitaAsmedianov <nikitaa@campus.technion.ac.il>
2026-06-29 11:50:39 +0300

    square equation

--- helloCPP.cpp
+++ helloCPP.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
 int main() {
     ifstream inFile("user.txt");
-    string line;
-    inFile>>line;
+    string names;
+    getline(inFile, names);
+    float num1, num2, num3;
+    inFile >> num1 >> num2 >> num3;
     inFile.close();
     ofstream outFile("hello.txt");
-    outFile <<"Hello, " << line << "!\n";
+    outFile << fixed << setprecision(2) << "Equation: " << num1 << "x^2+" << num2 << "x+" << num3 << "=0\n";
+    float D = num2 * num2 - 4 * num1 * num3;
+    outFile << "Discriminant: " << D << "\n";
+    float x1, x2;
+    if (D > 0) {
+        x1 = (-num2 + sqrt(D)) / (2 * num1);
+        x2 = (-num2 - sqrt(D)) / (2 * num1);
+        outFile << "Roots: " << x1 << ", " << x2 << "\n";
+    } else if (D == 0) {
+        x1 = -num2 / (2 * num1);
+        outFile << "Root: " << x1 << "\n";
+    } else {
+        outFile << "No real roots.\n";
+    }
     outFile.close();
     
-    cout << "File created and written successfully!\n";
+    cout << "Square equation processed successfully!\n";
     return 0;
 }
